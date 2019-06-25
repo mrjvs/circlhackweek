90f4e5603b4785db1178fc6d7c954f678452e8ad
mrjvs <jellevs@gmail.com>
2019-06-25 10:29:49 +0200

    dynamic command loading

--- commands/signup.js
+++ commands/signup.js
@@ -3,78 +3,83 @@ const utils = require("../utils.js");
 const constants = require("../constants.js");
 const randomString = require("randomstring");
 
-module.exports = async function (message, args) {
-    let userId = message.author.username.replace("[^\w]", "") + message.author.discriminator.replace("0", "");
-    let channel = await message.author.createDM();
 
-    let exists = await db.userModel.find({userId: userId});
-    if (exists.length !== 0) {
-        message.channel.send(utils.sendError("You already have an account, you cannot create another one."));
-        return;
-    }
-
-    let password = randomString.generate({
-        length: 7,
-        charset: "alphanumeric"
-    });
-
-    let uniqueIp = await db.ServerSchema.statics.generateUniqueIp();
-
-    let newServer = new db.serverModel({
-        ip: uniqueIp
-    });
-    newServer = await newServer.save();
-    const serverIp = newServer.ip;
-    const newUser = new db.userModel({
-        userId,
-        password,
-        serverIp
-    });
-
-    newUser.save(function (err, user) {
-        if (err) {
-            channel.send(utils.sendError("We failed to save your user data! ;("));
+module.exports = {
+    name: "signup",
+    aliases: [],
+    dmOnly: false,
+    execute: async function (message, args) {
+        let userId = message.author.username.replace("[^\w]", "") + message.author.discriminator.replace("0", "");
+        let channel = await message.author.createDM();
+    
+        let exists = await db.userModel.find({userId: userId});
+        if (exists.length !== 0) {
+            message.channel.send(utils.sendError("You already have an account, you cannot create another one."));
+            return;
         }
-    });
-
-    // Display information about the user: username + password
-    // Display information about what to do next
-    if (message.channel.type !== "dm") {
-        message.channel.send({
+    
+        let password = randomString.generate({
+            length: 7,
+            charset: "alphanumeric"
+        });
+    
+        let uniqueIp = await db.ServerSchema.statics.generateUniqueIp();
+    
+        let newServer = new db.serverModel({
+            ip: uniqueIp
+        });
+        newServer = await newServer.save();
+        const serverIp = newServer.ip;
+        const newUser = new db.userModel({
+            userId,
+            password,
+            serverIp
+        });
+    
+        newUser.save(function (err, user) {
+            if (err) {
+                channel.send(utils.sendError("We failed to save your user data! ;("));
+            }
+        });
+    
+        // Display information about the user: username + password
+        // Display information about what to do next
+        if (message.channel.type !== "dm") {
+            message.channel.send({
+                embed: {
+                    color: constants.embed_colors.info,
+                    title: "Thanks for signing up!",
+                    description: "We've sent you a DM with more information."
+                }
+            });
+        }
+        
+        channel.send({
             embed: {
-                color: constants.embed_colors.info,
-                title: "Thanks for signing up!",
-                description: "We've sent you a DM with more information."
+                title: "Welcome to Circl!",
+                description: "Put a description of Circl here.",
+                color: constants.embed_colors.info, //change this colour please @jvs
+                footer: {
+                    icon_url: "https://cdn.discordapp.com/embed/avatars/0.png",
+                    text: "Circl"
+                },
+                thumbnail: {
+                    url: "https://cdn.discordapp.com/embed/avatars/0.png"
+                },
+                author: {
+                    name: "Circl",
+                    icon_url: "https://cdn.discordapp.com/embed/avatars/0.png"
+                },
+                fields: [{
+                    name: "What is Circl?",
+                    value: "I honestly don't know."
+                },
+                {
+                    name: "Your User Information",
+                    value: `Username: \`${newUser.userId}\`\n Password: \`${newUser.password}\`\n Your IP: \`${newUser.serverIp}\``
+                }
+                ]
             }
         });
     }
-    
-    channel.send({
-        embed: {
-            title: "Welcome to Circl!",
-            description: "Put a description of Circl here.",
-            color: constants.embed_colors.info, //change this colour please @jvs
-            footer: {
-                icon_url: "https://cdn.discordapp.com/embed/avatars/0.png",
-                text: "Circl"
-            },
-            thumbnail: {
-                url: "https://cdn.discordapp.com/embed/avatars/0.png"
-            },
-            author: {
-                name: "Circl",
-                icon_url: "https://cdn.discordapp.com/embed/avatars/0.png"
-            },
-            fields: [{
-                name: "What is Circl?",
-                value: "I honestly don't know."
-            },
-            {
-                name: "Your User Information",
-                value: `Username: \`${newUser.userId}\`\n Password: \`${newUser.password}\`\n Your IP: \`${newUser.serverIp}\``
-            }
-            ]
-        }
-    });
-
 }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // libs
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const path = require('path');
 
 // config
 const config = require('./config.json');
@@ -11,6 +13,15 @@ db.init(config.connectionString);
 const signupCommand = require('./commands/signup.js')
 const stateMachine = require('./statemachine.js');
 
+// dynamic command loading
+const commands = [];
+const dir = fs.readdirSync('./commands');
+for (let i in dir) {
+    const com = require('./commands/' + dir[i]);
+    console.log(`Loaded command: ${com.name}.`);
+    commands.push(com);
+}
+
 client.on('ready', () => {
     console.log('Welcome to Circl!');
     console.log(`Logged in as ${client.user.tag}.`);
@@ -18,18 +29,27 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
     if (message.author.bot) return; // disallow bots from using commands
+    if (!message.content.startsWith(config.prefix)) return; // doesnt start with prefix so its not a command
 
-    // signup command is the only command allowed outside of DM's
-    if (message.content.startsWith(config.prefix + 'signup')) {
-        let args = message.content.split(" ");
-        args.shift();
-        return signupCommand(message, args);
+    const args = message.content.split(" ");
+    const messageCommand = args.shift().substring(config.prefix.length); // remove prefix from text
+
+    // check if text is a command
+    for (let i in commands) {
+        // check if command is only for dm's
+        if (message.channel.type === 'dm' && commands[i].dmOnly || !commands[i].dmOnly) {
+            if (messageCommand === commands[i].name) {
+                commands[i].execute(message, args);
+            } else {
+                // check aliases
+                for (let aliasIndex in commands[i].aliases) {
+                    if (messageCommand === commands[i].aliases[aliasIndex]) {
+                        commands[i].execute(message, args);
+                    }
+                }
+            }
+        }
     }
-
-    if (message.content.startsWith(config.prefix) && message.channel.type == 'dm') {
-        
-    }
-
 });
 
 client.login(config.token);
